f8f2902398212b4ba38b310510c464717dd11679
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-05-28 21:17:50 -0500

    hides working

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import util from '../../helpers/util';
+import googleImg from './signIn.png';
+
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const authStringBuilder = () => {
+  let domString = '<button id="google-auth" class="btn btn-info">';
+  domString += `<img src=${googleImg}/>`;
+  domString += '</button>';
+  util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
+};
+
+export default { authStringBuilder };
